3f6d0d653398532ba8d3e5227c95c8e8769cb4a2
Ye Chao <yec@glodon.com>
2016-07-04 18:28:11 +0800

    zhihu

--- zhihu/zhihu.py
+++ zhihu/zhihu.py
@@ -9,19 +9,21 @@ chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
 chrome = webdriver.Chrome(chrome_options=chrome_options)
 chrome.get("https://www.zhihu.com")
-name_el = chrome.find_element_by_css_selector('.name.input-wrapper input')
-email_el = chrome.find_element_by_css_selector('.email.input-wrapper input')
-password_el = chrome.find_element_by_css_selector('input[name="password"]')
-captcha_el = chrome.find_element_by_css_selector('input[name="captcha"]')
-submit_el = chrome.find_element_by_css_selector('.sign-button.submit')
-print name_el, email_el, password_el, captcha_el, submit_el,
 
+# inject jquery
 jquery_str = ''
 with open(JQUERY_FILE, 'r') as f:
     jquery_str = f.read()
-
-print jquery_str
-
 chrome.execute_script(jquery_str)
-name_str = "$('.name.input-wrapper input').val('asdasdasd1was%s')" % random.randrange(10000)
+
+captcha = raw_input('captcha:')
+name_str = "$('.view-signup input[name=\"fullname\"]').val('asdasdasd1was%s')" % random.randrange(10000)
+email_str = "$('.view-signup input[name=\"account\"]').val('asdasdasd1was%s@qq.com')" % random.randrange(10000)
+password_str = "$('.view-signup input[name=\"password\"]').val('111111')"
+captcha_str = "$('.view-signup input[name=\"captcha\"]').val('%s')" % captcha
+submit_str = "$('.view-signup .sign-button.submit').click()"
 chrome.execute_script(name_str)
+chrome.execute_script(email_str)
+chrome.execute_script(password_str)
+chrome.execute_script(captcha_str)
+chrome.execute_script(submit_str)
